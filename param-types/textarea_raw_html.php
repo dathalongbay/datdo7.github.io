05cb8d70feaa029482134392c49bc15576bb0e73
datdo <dathalongbay@gmail.com>
2017-08-31 09:58:59 +0700

    add editor

---
+++ param-types/textarea_raw_html.php
@@ -0,0 +1,9 @@
+<?php
+array(
+    'type' => 'textarea_raw_html',
+    'holder' => 'div',
+    'heading' => __( 'Raw HTML', 'js_composer' ),
+    'param_name' => 'content',
+    'value' => base64_encode( '<p>I am raw html block.<br/>Click edit button to change this html</p>' ),
+    'description' => __( 'Enter your HTML content.', 'js_composer' ),
+);
---
+++ param-types/widgetised_sidebars.php
@@ -0,0 +1,7 @@
+<?php
+array(
+    'type' => 'widgetised_sidebars',
+    'heading' => __( 'Sidebar', 'js_composer' ),
+    'param_name' => 'sidebar_id',
+    'description' => __( 'Select widget area to display.', 'js_composer' ),
+);
