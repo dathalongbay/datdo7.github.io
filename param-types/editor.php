4cec446f945abe11d1fb82440d234d3fcbfa479a
datdo <dathalongbay@gmail.com>
2017-08-31 09:09:57 +0700

    add editor

---
+++ param-types/editor.php
@@ -0,0 +1,8 @@
+<?php
+array(
+    'type' => 'textarea_html',
+    'holder' => 'div',
+    'heading' => __( 'Text 111', 'js_composer' ),
+    'param_name' => 'content',
+    'value' => __( '<p>I am text block. Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>', 'js_composer' ),
+);
