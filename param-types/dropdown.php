6c56f9106050a822b573ef9695a981e319db29be
datdo <dathalongbay@gmail.com>
2017-08-31 09:15:53 +0700

    add editor

---
+++ param-types/dropdown.php
@@ -0,0 +1,12 @@
+<?php
+array(
+    'type' => 'dropdown',
+    'heading' => __( 'Text source', 'js_composer' ),
+    'param_name' => 'source',
+    'value' => array(
+        __( 'Custom text', 'js_composer' ) => '',
+        __( 'Post or Page Title', 'js_composer' ) => 'post_title',
+    ),
+    'std' => '',
+    'description' => __( 'Select text source.', 'js_composer' ),
+);
