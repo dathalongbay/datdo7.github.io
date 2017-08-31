cc7048d2609158c8c9234ec54c86cfe1138b6fb8
datdo <dathalongbay@gmail.com>
2017-08-31 09:19:39 +0700

    add editor

---
+++ param-types/checkbox.php
@@ -0,0 +1,8 @@
+<?php
+array(
+    'type' => 'checkbox',
+    'heading' => __( 'Use theme default font family?', 'js_composer' ),
+    'param_name' => 'use_theme_fonts',
+    'value' => array( __( 'Yes', 'js_composer' ) => 'yes' ),
+    'description' => __( 'Use font family from the theme.', 'js_composer' ),
+);
---
+++ param-types/link.php
@@ -0,0 +1,8 @@
+<?php
+array(
+    'type' => 'vc_link',
+    'heading' => __( 'URL (Link)', 'js_composer' ),
+    'param_name' => 'link',
+    'description' => __( 'Add link to custom heading.', 'js_composer' ),
+    // compatible with btn2 and converted from href{btn1}
+);
